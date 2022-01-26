797208efd53b15f7f3a5a408b27615bed330b058
Martin Holdrege <martinholdrege@gmail.com>
2022-01-26 10:55:04 -0700

    WIP

--- scripts/04_interpolated_maps.R
+++ scripts/04_interpolated_maps.R
@@ -6,29 +6,38 @@
 # based on STEPWAT2 output for 200 sites (for each plant functional
 # type and climate and grazing scenario) that has been upscaled across
 # the west. The rasters of interpolated data are created
-# in the 03_interpolate.R script, which can takes ~ an hour to run on
-# my laptop
+# in the 03_interpolate.R script, which can takes ~ 4 hours to run on
+# my laptop (when excluding RCP4.5)
 
 
 # dependencies ------------------------------------------------------------
 
-library(tidyverse)
+library(tidyverse) 
 library(terra)
 source("src/mapping_functions.R")
 source("src/general_functions.R")
 
 # read in data ------------------------------------------------------------
 
-bio_files <- list.files("data_processed/interpolated_rasters/biomass/",
-                        pattern = ".tif$",
-                        full.names = TRUE)
 
-bio_d_files <- list.files("data_processed/interpolated_rasters/bio_diff/",
-                        pattern = ".tif$",
+# * rasters ---------------------------------------------------------------
+
+# data up-scaled for each GCM, and for c4on and c4off
+bio_files <- list.files("data_processed/interpolated_rasters/biomass/",
+                        pattern = "^c4.*tif$",
                         full.names = TRUE)
+length(bio_files)
+# bio_d_files <- list.files("data_processed/interpolated_rasters/bio_diff/",
+#                         pattern = ".tif$",
+#                         full.names = TRUE)
+
+
+# for now just using a subset for computational efficiency
+bio_files <- bio_files[sample(1:length(bio_files), 200)]
 
 # this is kind of a 'raster stack', where each layers is a tif
-rast1 <- terra::rast(c(bio_files, bio_d_files)) # class SpatRast
+# it's not actually loaded into memory
+rast1 <- terra::rast(bio_files) # class SpatRast
 
 
 # * AIM -------------------------------------------------------------------
@@ -52,6 +61,10 @@ heights6 = rep(1, 2)
 wfig6 <- 9 # width of 9 figure panels (inches)
 hfig6 <- 6 # height of 9 figure panels
 
+# some terra operations can be done in parallel and need 
+# to know num of cores
+num.cores <- parallel::detectCores(logical = FALSE) 
+
 # names of layers --------------------------------------------------
 # descriptions of layers of raster stack
 r_names <- names(rast1)
@@ -59,13 +72,59 @@ r_names <- names(rast1)
 names_df <- tibble(id2 = r_names,
                    id = r_names) %>% 
   separate(col = id2,
-           into = c("PFT", "type", "RCP", "years", "graze"),
+           into = c("c4", "PFT", "type", "RCP", "years", "graze", "GCM"),
            sep = "_") %>% 
   mutate(layer_num = 1:nrow(.),
          graze = graze2factor(graze),
          years = years2factor(years),
          RCP = rcp2factor(RCP),
-         PFT = pft5_factor(PFT))
+         PFT = pft_all_factor(PFT),
+         # remove GCM from the string
+         id_noGCM = str_replace(id, "_[^_]*$", "")) %>% 
+  # the ordering is important for later creation of spatraster dataset
+  arrange(id)
+
+
+# median by GCM -----------------------------------------------------------
+# for each treatment/scenario combination calculate the median
+# across GCMs
+
+
+
+# create SpatRasterDataset
+
+rast2 <- rast1[[names_df$id]] # make sure rows are ordered correctly
+
+# list of rasters, each element is a SpatRaster including all GCMs
+# for a given treatment/scenario combination
+rast_gcm_l <- split(rast2, f = names_df$id_noGCM)
+
+names(rast_gcm_l) <- unique(names_df$id_noGCM)
+
+# SpatRasterDataset (i.e. each dataset includes all GCMs for treatment/scenario)
+sds_gcm <- sds(rast_gcm_l) 
+class(sds_gcm)
+
+# check that the number of layers in each dataset is correct
+stopifnot(nlyr(sds_gcm) == names_df %>% 
+            group_by(id_noGCM) %>% 
+            summarise(n = n()) %>% 
+            pull(n)
+          )
+
+# compute median across GCMs
+system.time(
+  sds_med1 <- terra::app(sds_gcm, f = "median", cores = num.cores)
+)
+
+
+
+# NEXT--take medians across GCM's in some kind of sensible way
+# First make them into a spatrasterdataset (sds), then use app(x, median), 
+# to take the median of each dataset
+# m
+
+
 
 # names of layers for first set of of maps
 # just want to compare current biomass to delta biomass in the future under
@@ -106,8 +165,8 @@ image_bio_diff(rast1, subset = 150, title = "test2")
 # current for that same grazing level.
 # RCP 4.5 and 8.5 shown seperately
 
-pdf("figures/biomass_maps/bio_current_vs_delta_by_graze_v1.pdf",
-  width = wfig6, height = hfig6)
+# pdf("figures/biomass_maps/bio_current_vs_delta_by_graze_v1.pdf",
+#   width = wfig6, height = hfig6)
 
 
 par(mar = mar, mgp = mgp)
@@ -187,8 +246,8 @@ lyrs_current <- map(levs_pft, function(pft) {
 # Actual biomass (light grazing), and delta biomass for moderate,
 # heavy and very heavy grazing
 
-pdf("figures/biomass_maps/bio_current-light_and_delta-current_by_graze_v1.pdf",
-    width = wfig6, height = hfig6)
+# pdf("figures/biomass_maps/bio_current-light_and_delta-current_by_graze_v1.pdf",
+#     width = wfig6, height = hfig6)
 
 
 # 'looping' over PFT's
@@ -248,8 +307,8 @@ aim_sv1 <- vect(aim2, geom = c("longitude", "latitude"))
 # * maps ------------------------------------------------------------------
 
 # Next--separate maps showing the 200 sites, and c4 presence/absence
-pdf("figures/biomass_maps/C4Pgrass-bio_AIMC4-Pgrass-presence.pdf",
-    width = wfig6, height = hfig6)
+# pdf("figures/biomass_maps/C4Pgrass-bio_AIMC4-Pgrass-presence.pdf",
+#     width = wfig6, height = hfig6)
 
 pch1 <- 16
 cex1 <- 0.6
