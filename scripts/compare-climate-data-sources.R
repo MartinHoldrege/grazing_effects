f400317699bd592b54c3d281a944537befc62ef5
Martin Holdrege <martinholdrege@gmail.com>
2024-06-03 12:29:21 -0600

    added scatterplots that now comparel ~all data sources (incl gridmet)

--- scripts/compare-climate-data-sources.R
+++ scripts/compare-climate-data-sources.R
@@ -35,6 +35,22 @@ rr_fut1 <- rast(file.path(path_large, 'newRR3-analysis/examples/tmp_examples',
 
 # climate from the weather data base, summarized in the 00_query_weather_db.R script
 db_clim1 <- read_csv('data_processed/site_means/dbWeather_200sites.csv')
+
+
+# mean montly precip, and min and max temp, from daymet.
+# calculated in the daymet_normals.js script, in the SEI repository
+day1 <- rast("data_raw/daymet_monthly_normals_1981-2010.tif")
+
+
+# * grid-met --------------------------------------------------------------
+# ~ 4 km gridded data (from Daniel)
+grid1 <- rast(file.path(
+  path_large,
+  "climate_data/gridmet-clim",
+  c('gridmet_ppt_annualClimatologies.nc', 'gridmet_tmmn_annualClimatologies.nc',
+    'gridmet_tmmx_annualClimatologies.nc')
+))
+
 # summarize across GCMs ---------------------------------------------------
 
 sw_clim_med <- sw_clim1 %>% 
@@ -44,7 +60,7 @@ sw_clim_med <- sw_clim1 %>%
   # PTcor2 is the mean of yearly correlations
   summarise(PTcor_stepwat = median(PTcor),
             MAT_stepwat = median(MAT),
-            MAP_stepwat = median(MAP)) # median across GCMs
+            MAP_stepwat = median(MAP)*10) # median across GCMs, convert to mm
 # extract clim at points --------------------------------------------------
 
 # convert site locations to sf object
@@ -62,10 +78,10 @@ terra::activeCat(rr_cur1) <- 'CorTempPPT_mean'
 
 rr_fut2 <- terra::extract(rr_fut1, loc2) %>% 
   mutate(RCP = 'RCP45') %>% 
-  rename(PTcor_rr = CorTempPPT_mean_acrmoddistr)
+  rename(PTcor2_rr = CorTempPPT_mean_acrmoddistr)
 rr_cur2 <- terra::extract(rr_cur1, loc2) %>% 
   mutate(RCP = 'Current') %>% 
-  rename(PTcor_rr = CorTempPPT_mean)
+  rename(PTcor2_rr = CorTempPPT_mean)
 
 
 # * MAP -------------------------------------------------------------------
@@ -80,6 +96,21 @@ rr_cur2a <- terra::extract(rr_cur1, loc2) %>%
   mutate(RCP = 'Current') %>% 
   rename(MAP_rr = PPT_mean)
 
+# * MAT -------------------------------------------------------------------
+
+terra::activeCat(rr_fut1) <- 'Tmean_mean_acrmoddistr'
+terra::activeCat(rr_cur1) <- 'Tmean_mean'
+
+rr_fut2b <- terra::extract(rr_fut1, loc2) %>% 
+  mutate(RCP = 'RCP45') %>% 
+  rename(MAT_rr = Tmean_mean_acrmoddistr)
+rr_cur2b <- terra::extract(rr_cur1, loc2) %>% 
+  mutate(RCP = 'Current') %>% 
+  rename(MAT_rr = Tmean_mean)
+
+
+# * database --------------------------------------------------------------
+
 
 stopifnot(! 'RCP' %in% names(db_clim1)) # db_clim1 is just for current climate
 db_clim2 <- db_clim1 %>% 
@@ -87,58 +118,95 @@ db_clim2 <- db_clim1 %>%
   rename( 'MAT_db' = 'MAT_C', 'MAP_db' = 'MAP_mm', PTcor2_db = 'CorrTP2',
           PTcor_db = 'CorrTP') %>% 
   mutate('RCP' = 'Current')
-comb <- sw_clim_med %>% 
-  left_join(rr_fut2, by = c('site' = 'ID', 'RCP'))%>% 
-  left_join(rr_cur2, by = c('site' = 'ID', 'RCP')) %>% 
-  mutate(PTcor_rr = ifelse(is.na(PTcor_rr.x), PTcor_rr.y, PTcor_rr.x)) %>% 
-  left_join(bind_rows(rr_fut2a, rr_cur2a), by = c('site' = 'ID', 'RCP')) %>% 
-  select(-PTcor_rr.y, -PTcor_rr.x) %>% 
-  left_join(db_clim2, by =  c('site' = 'Site_id', 'RCP'))
 
+
+# *gridmet ----------------------------------------------------------------
+
+# some how x and y coords got reversed
+grid2 <- terra::project(grid1, crs(grid1)) %>% 
+  t() %>% 
+  flip(direction = 'vertical') %>% 
+  flip(direction = 'horizontal')
+grid3 <- terra::project(grid2, crs(loc2))
+
+grid4 <- terra::extract(grid3, loc2)
+grid5 <- grid4 %>% 
+  mutate(MAT_grid = (tmmn + tmmx)/2 -273.15,
+         RCP = 'Current') %>% 
+  rename('MAP_grid' = 'ppt') %>% 
+  select(-tmmn, -tmmx) 
+
+# * daymet ----------------------------------------------------------------
+
+day2 <- terra::extract(day1, loc2)
+
+day3 <- day2 %>% 
+  pivot_longer(cols = -ID,
+               names_to = c("name", 'month'),
+               names_sep = "_") %>% 
+  pivot_wider(values_from = 'value',
+              names_from = 'name') %>% 
+  mutate(tmean = (tmax + tmin)/2) %>% 
+  group_by(ID) %>% 
+  summarize(PTcor_day = cor(tmean, prcp),
+            MAP_day = sum(prcp),
+            MAT_day = mean(tmean)) %>% 
+  mutate(RCP = 'Current')
+
+
+# * combine ---------------------------------------------------------------
+
+comb <- sw_clim_med %>% 
+  left_join(bind_rows(rr_fut2, rr_cur2), by = c('site' = 'ID', 'RCP'))%>% 
+  left_join(bind_rows(rr_fut2a, rr_cur2a), by = c('site' = 'ID', 'RCP')) %>% 
+  left_join(bind_rows(rr_fut2b, rr_cur2b), by = c('site' = 'ID', 'RCP')) %>% 
+  left_join(db_clim2, by =  c('site' = 'Site_id', 'RCP')) %>% 
+  left_join(day3, by = c('site' = 'ID', 'RCP')) %>% 
+  left_join(grid5, by = c('site' = 'ID', 'RCP'))
+
+comb_long <- comb %>% 
+  #filter(RCP == 'Current') %>% 
+  pivot_longer(names_to = c('variable', 'data_source'),
+               cols = -c(site, years, RCP),
+               names_sep = "_") %>% 
+  pivot_wider(names_from = 'data_source') %>% 
+  ungroup()
 
 # figures -----------------------------------------------------------------
 
-
 # *comparing stepwat weather to gridmet -----------------------------------
 
-
-cap1 = 'The STEPWAT2 (type 1) and the GridMet/MACA P-T correlations (type 2)'
 cap2 <- "'type 2' is the correlation between monthly values in a given yr, then avaraged\n type 1 is correlation of monthly means"
 
-g <- ggplot(comb, aes(PTcor_rr, PTcor_stepwat)) +
-  geom_point() +
-  facet_wrap(~RCP) +
-  labs(x = 'GridMet/MACA (type 2)',
-       y = 'STEPWAT2 (type 1)',
-       caption = cap1,
-       subtitle = 'Correlation between monthly temperature and precip,\nfor 200 sites at which STEPWAT simulations are conducted')
 
 g0 <- ggplot(comb, aes(PTcor_stepwat)) +
   geom_histogram() +
   facet_wrap(~RCP)+
-  labs(x = 'PT correlation (STEPWAT2)')
+  labs(x = 'PT correlation (STEPWAT2, type 1)')
 
-g1 <- ggplot(comb, aes(PTcor_rr)) +
+g1 <- ggplot(comb, aes(PTcor2_rr)) +
   geom_histogram() +
   facet_wrap(~RCP) +
-  labs(x = 'PT correlation (MACA/GridMet)')
+  labs(x = 'PT correlation (RR dataset, type 2)')
 
 g3 <- g0/g1
 
-g4 <- ggplot(comb, aes(PTcor_rr, PTcor2_db)) +
+g4 <- ggplot(comb, aes(PTcor2_rr, PTcor2_db)) +
   geom_point() +
   labs(x = 'GridMet/MACA (type 2)',
        y = 'Weather database (from 200 sites, type 2)',
        caption = cap2,
        subtitle = 'Correlation (type 2) between monthly temperature and precip,\nfor 200 sites at which STEPWAT simulations are conducted') +
   geom_abline(slope = 1)
-g4
 
-g5 <- ggplot(comb, aes(MAP_rr, MAP_stepwat*10)) +
+g5 <- ggplot(comb, aes(PTcor_db, PTcor2_db)) +
   geom_point() +
-  labs(x = 'MAP from GridMet/MACA',
-       y = 'MAP from STEPWAT')+
+  labs(x = 'weather database (type 1)',
+       y = 'weather database (type 2)',
+       caption = cap2,
+       subtitle = 'Correlation (type 2) between monthly temperature and precip,\nfor 200 sites at which STEPWAT simulations are conducted') +
   geom_abline(slope = 1)
+
 # *comparing weather db to stepwat output ----------------------------------
 # the climate from the weather db should be closely comparable to what comes
 # out of stepwat (which has gone through the wx generator)
@@ -151,18 +219,11 @@ base <- function() {
   )
 }
 
+pdf('figures/climate/clim_comparison-between-sources.pdf')
 
-
-pdf('figures/climate/PTcor_comparison-between-sources.pdf')
-g
 g3
 g4
 g5
-ggplot(comb, aes(MAP_db, MAP_stepwat*10)) +
-  base()
-
-ggplot(comb, aes(MAT_db, MAT_stepwat)) +
-  base()
 
 ggplot(comb, aes(PTcor_db, PTcor_stepwat))+
   base() +
@@ -183,6 +244,89 @@ ggplot(comb, aes(PTcor_db, PTcor2_db))+
      x = 'PT cor from database (type 1)',
      y = 'PT cor from database (type 2)',
      subtitle = 'weather db data (comparing summary methods)')
+
+# * compare all datasets ------------------------------------------
+
+comb_long %>% 
+  select(variable, stepwat, day, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(day, stepwat)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free',
+             ncol = 2) +
+  geom_abline(slope = 1) +
+  labs(x = 'Daymet V3',
+       y = 'STEPWAT')
+
+comb_long %>% 
+  select(variable, stepwat, rr, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(rr, stepwat)) +
+  geom_point() +
+  facet_wrap(RCP~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'RR dataset',
+       y = 'STEPWAT')
+
+comb_long %>% 
+  select(variable, stepwat, grid, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(grid, stepwat)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'GridMet',
+       y = 'STEPWAT')
+
+comb_long %>% 
+  select(variable, stepwat, db, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(db, stepwat)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'Database for STEPWAT',
+       y = 'STEPWAT')
+
+comb_long %>% 
+  select(variable, day, db, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(day, db)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'DayMet V3',
+       y = 'Database for STEPWAT')
+
+comb_long %>% 
+  select(variable, grid, day, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(day, grid)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'DayMet V3',
+       y = 'GridMet')
+
+comb_long %>% 
+  select(variable, rr, day, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(rr, day)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'RR dataset',
+       y = 'Daymet V3')
+
+comb_long %>% 
+  select(variable, rr, grid, RCP) %>% 
+  drop_na() %>% 
+  ggplot(aes(rr, grid)) +
+  geom_point() +
+  facet_wrap(~variable, scales = 'free') +
+  geom_abline(slope = 1) +
+  labs(x = 'RR dataset',
+       y = 'GridMet')
 
 dev.off()
 
