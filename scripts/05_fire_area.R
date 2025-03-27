f28468b567dd407212d1bb6ab7ea3ec90430246d
Martin Holdrege <martinholdrege@gmail.com>
2025-03-27 16:58:18 -0600

    calculated the expected area falling into different 'age groups' (since fire)

--- scripts/05_fire_area.R
+++ scripts/05_fire_area.R
@@ -1,6 +1,5 @@
 # Purpose: Calculate the expected burned area, and the amount of
-# area in different categories of fire return intervals (which we
-# consider a proxy for the potential sagebrush 'stand' age in that location)
+# area in different categories of expected stand age (based on fire probability)
 
 # Author: Martin Holdrege
 
@@ -19,6 +18,17 @@ v <- 'v1' # version for output files
 bp_bins <- c(Inf, 3, 2, 1, 0.5, 0) # neg probability in last bin so includes 0
 fri_labels <- c("< 33", '33 - 50', '50 - 100', '100 - 200', '200 +')
 
+# for area by age class group
+# these intervals of years
+# to answer the question of (e.g) what is the expected area of the region
+# that will have burned between 0 and 10 years ago
+age_groups <- list(
+  c(a = 0, b = 10),
+  c(a = 11, b = 25),
+  c(a = 26, b = 75),
+  c(a = 76, b = 150),
+  c(a = 151, b = Inf)
+)
 
 # dependencies ------------------------------------------------------------
 
@@ -26,6 +36,7 @@ library(terra)
 library(tidyverse)
 source("src/general_functions.R")
 source("src/mapping_functions.R")
+source("src/probability_functions.R")
 
 # read in data ------------------------------------------------------------
 
@@ -45,6 +56,25 @@ info_prob1 <- create_rast_info(r_prob1, into = into)
 eco1 <- load_wafwa_ecoregions()
 
 
+
+# vectors etc -------------------------------------------------------------
+
+names(age_groups) <- map_chr(age_groups, function(x) {
+  out <- if(is.finite(x['b'])) {
+    paste0(x['a'], '-',  x['b'])
+  } else {
+    paste0(x['a'], ' +')
+  }
+  out
+})
+
+# the probability of falling into any one of the categories should be 1
+# (b/ the categories include all possible years)
+test <- map_dbl(age_groups, function(x) {
+  prob_geometric_rule(p = 0.01, a = x['a'], b = x['b'])
+})
+stopifnot(all.equal(sum(test), 1))
+
 # prepare rasters ---------------------------------------------------------
 
 stopifnot(isTRUE(same.crs(r_prob1, vect(eco1))))
@@ -61,9 +91,8 @@ area <- cellSize(r_prob1[[1]], unit = 'ha', transform = FALSE)
 varnames(area) <- 'cellSize'
 # amount of area expected to burn in each
 # first convert probability from % to proportion
-r_area_exp <- (r_prob1/100)*area 
-
-
+r_prob2 <- r_prob1/100 
+r_area_exp <- r_prob2*area 
 
 # expected burned area per ecoregion
 ba_eco1 <- terra::extract(
@@ -82,56 +111,61 @@ ba_eco2 <- ba_eco1 %>%
                values_to = 'area') %>% 
   left_join(info_prob1, by = 'id')
 
+# area by by stand age ----------------------------------------------------
 
-
-# area by fire return interval bin ----------------------------------------
-
-# Use terra::classify to bin probabilities
-
-rcl <- cbind(bp_bins[-1], # from
-             bp_bins[-length(bp_bins)],  # to
-             1:length(fri_labels)) # becomes
-
-r_binned <- terra::classify(r_prob1, rcl = rcl,
-                            right = FALSE)
-
-# calculates the area in each combination of 
-# binned fire probability group and ecoregion
-fri_area_l <- map(names(r_binned), function(lyr) {
-  df <- zonal(area, c(r_binned[[lyr]], r_eco1), 
-              fun = 'sum', wide = FALSE)
-  df$fri_group <- df[[lyr]]
-  df$id <- lyr
-  df[[lyr]] <- NULL
-  df
+# probability of each pixel of falling into a given age group
+prob_by_age_group_l <- map(age_groups, function(x) {
+  prob_geometric_rule(r_prob2, a = x['a'], b = x['b'])
 })
 
-fri_area1 <- fri_area_l %>% 
-  bind_rows() %>% 
-  as_tibble()
- 
-# adding 0s ---------------------------------------------------------------
+# excpecte area (for each pixel) by age group
+area_by_age_group_l <- map(prob_by_age_group_l, \(x) x*area)
 
-# adding missing (zero area) back in
+area_age_group1 <- map(area_by_age_group_l, 
+                       \(x) terra::extract(x, vect(eco1), fun = sum, 
+                                           na.rm = TRUE))
 
-cols2expand <- map(fri_area1[c("fri_group", "ecoregion", "id")], unique) 
+
+area_age_group2 <- map(area_age_group1, function(x) {
+  x$ecoregion <- eco1$ecoregion[x$ID]
+  x$ID <- NULL
   
-expanded <- expand_grid(!!!cols2expand)
-  
-fri_area2 <- fri_area1 %>% 
-  right_join(expanded,
-             by = join_by(ecoregion, fri_group, id)) %>% 
-  mutate(area = ifelse(is.na(area), 0, area),
-         fri_label = fri_labels[fri_group]) %>% 
-  left_join(info_prob1, by = 'id') %>% 
-  select(-id)
-  
+  x %>% 
+    pivot_longer(cols = -ecoregion,
+               names_to = 'id',
+               values_to = 'area') 
+    
+}) %>% 
+  bind_rows(.id = 'age_group') %>% 
+  left_join(info_prob1, by = 'id')
+
+
+# area by ecoregion -------------------------------------------------------
+# for % of area calculations
+mask <- r_prob1[[1]]
+mask[!is.na(tmp)] <- 1
+area_masked <- mask(area, mask)
+
+area_eco <- terra::extract(
+  area_masked,
+  vect(eco1),         
+  fun = sum,
+  na.rm = TRUE
+)
+area_eco$ecoregion <- eco1$ecoregion
+area_eco$ID <- NULL
+
 # save output -------------------------------------------------------------
 
-write_csv(ba_eco2, 
-          paste0("data_processed/area/expected-burn-area_", v, "_", run, 
-                 ".csv"))
-
-write_csv(fri_area2, 
-          paste0("data_processed/area/area-by-fri-class_", v, "_", run, 
-                 ".csv"))
+if(!test_run) {
+  write_csv(ba_eco2, 
+            paste0("data_processed/area/expected-burn-area_", v, "_", run, 
+                   ".csv"))
+  
+  write_csv(area_eco, 
+            paste0("data_processed/area/ecoregion-area_", v,".csv"))
+  
+  write_csv(area_age_group2, 
+            paste0("data_processed/area/area-by-age-group_", v, "_", run, 
+                   ".csv"))
+}
---
+++ src/probability_functions.R
@@ -0,0 +1,108 @@
+# functions related to probability calculations 
+
+
+#' probability have being between a and b years post fire
+#' 
+#' @description
+#' This function is meant to answer the question, given a particular
+#' fire probability, what is the probability that a observation (location in space or time)
+#' has not had fire for between a and b years
+#' 
+#'
+#' @param p probability of fire in a given year, a vector, or a spatraster
+#' @param a start of the period of interest (inclusive)
+#' @param b end of the period of interest
+prob_geometric_rule <- function(p, a = 0, b) {
+  stopifnot(a <= b,
+            a >= 0,
+            any(class(p) %in% c('numeric', 'SpatRaster')))
+  
+  # making sure p could be a probability
+  if(class(p) == 'numeric') {
+    stopifnot(p>= 0 & p<=1)
+  } else {
+    mm <- as.numeric(minmax(p))
+    stopifnot(mm[1] >= 0 & mm[2] <=1)
+  }
+
+  Pnf <- 1 - p # probability of no fire in a given year
+  
+  # probability of having a sequence of between a and b years of no fire
+  # formula based on the 2nd proof provided here:
+  # https://math.stackexchange.com/questions/971761/calculating-sum-of-consecutive-powers-of-a-number
+  # note that instead of summing from 0 to n (as in that example),
+  # I'm want to be able to sum from some number (a) to b, so the 
+  # 'previous' (a - 1) sums are subtracted
+  prob_a_b <- (1 - Pnf^(b + 1))/(1 - Pnf) - (1 - Pnf^a)/(1 - Pnf)
+  
+  prob_a_b*p # the sequence must occur after a fire
+}
+
+if (FALSE) {
+  n <- 1e6
+  
+  p1 <- 0.01
+  ps <- rbeta(n = n, shape1 = 0.01, shape2 = 0.99)
+  mean(ps)
+  
+  # showing that even when each probability is different (but has same mean)
+  # the lengths of no fire periods are very close to
+  # those when the probability is fixed at the mean)
+  x <- runif(n = n)
+  hasNoFire1 <- x > p1
+  res1 <- rle(hasNoFire1)
+  # see below, this doesn't deal with 0 length sequences, so is approximate
+  mean(res1[["lengths"]][res1[["values"]]] > 100) 
+  hist(res1[["lengths"]][res1[["values"]]])
+  mean(res1[["lengths"]][res1[["values"]]])
+  
+  mean(hasNoFire1)
+  
+  hasNoFire2 <- x > ps
+  #hasNoFire2 <- rbinom(n = n, size = 1, prob = 1 - ps)
+  hist(ps); summary(ps)
+  res2 <- rle(hasNoFire2)
+  hist(res2[["lengths"]][res2[["values"]]])
+  mean(res2[["lengths"]][res2[["values"]]])
+  # see below, this doesn't deal with 0 length sequences, so is approximate
+  mean(res2[["lengths"]][res2[["values"]]] > 100) # fraction of time/space 
+  mean(hasNoFire2)
+  
+  # showing that the function recreates the simulated values
+  
+  k <- 100
+  lengths100 <- vector(length = k)
+  lengths26_50 <- vector(length = k)
+  for (i in 1:k) {
+    x <- runif(n = n)
+    hasNoFire1 <- x > p1
+    res1 <- rle(hasNoFire1)
+    lts <- res1[["lengths"]]
+    lts[(lts == 1 & !res1[["values"]])] <- NA # single year that burned
+    
+    # this is year that burned, when the previous year burned
+    # so this could be considered a unburned sequence of length 0.
+    lts[(lts > 1 & !res1[["values"]])] <- 0
+    lts <- lts[!is.na(lts)]
+    
+    lengths100[i] <- mean(lts > 100)
+    # probability of a sequence having had no fire for 26-50 years
+    lengths26_50[i] <- mean(lts >= 26 & lts <= 50)
+  }
+  
+  # probability of being in a place/time where fire has not occurred for
+  # over 100 years
+  p100 <- 1 - prob_geometric_rule(p1, a = 0, b = 100)
+  hist(lengths100)
+  abline(v = p100, col = 'blue') # theoretical mean
+  abline(v = mean(lengths100), col = 'red', lty = 2) # simulated mean
+  
+  # probability of being in a place/time where fire has has not occurred
+  # in 26-50 years
+  p26_50 <- prob_geometric_rule(p1, a = 26, b = 50)
+  hist(lengths26_50); 
+  abline(v = p26_50, col = 'blue') # theoretical mean
+  abline(v = mean(lengths26_50), col = 'red', lty = 2) # simulated mean
+}
+
+
