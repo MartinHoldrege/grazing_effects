bb685dd454c42c50c0b487a32653ec8376e591b9
Martin Holdrege <martinholdrege@gmail.com>
2025-08-22 17:05:16 -0600

    corrected gcm-wise summaries to be based on % change

--- scripts/07_fire_area_summaries.R
+++ scripts/07_fire_area_summaries.R
@@ -43,7 +43,7 @@ ba_gcm2 <- ba_gcm1 %>%
 
 # b/ comparing across grazing scenarios, 
 # comparing within a gcm (not -pixelwise gcm summaries) b/
-# doesn't  make sense to compare different grazing scenarios
+# doesn't  make sense to compare different grazing scenarios across differnt gcms
 # 
 ba_gcm_delta_graze1 <- ba_gcm2 %>% 
   filter(graze == graze_ref) %>% 
@@ -51,9 +51,8 @@ ba_gcm_delta_graze1 <- ba_gcm2 %>%
   select(-graze) %>% 
   right_join(filter(ba_gcm2, graze != graze_ref),
              by = join_by(ecoregion, run, RCP, years, GCM)) %>% 
-  mutate(ba_delta = ba_ref - ba,
-         ba_delta_perc = round(ba_delta/ba_ref*100, 1),
-         ba_delta = round(ba_delta))
+  mutate(ba_delta = ba - ba_ref,
+         ba_delta_perc = ba_delta/ba_ref*100)
 
 ba_smry2 <- ba_smry1 %>%  
   select(-run2, -matches('_perc')) %>% 
@@ -77,22 +76,44 @@ ba_smry_delta_clim1 <- ba_smry2 %>%
 # summarize across GCMs ---------------------------------------------------
 
 ba_smry_delta_graze1 <- ba_gcm_delta_graze1 %>% 
-  select(-ba_ref) %>% 
   group_by(ecoregion, run, RCP, years, graze) %>% 
-  summarize(across(.cols = c('ba_delta', 'ba_delta_perc'),
+  summarize(across(.cols = c('ba_delta_perc'),
                    .fns = list(
                      'median' = median,
                      'low' = calc_low,
                      'high' = calc_high
-                   )))
+                   ))) 
+  
+join_vars <- c('ecoregion', 'run', 'RCP', 'years', 'graze')
+tmp <- select(ba_gcm_delta_graze1, all_of(join_vars), ba_delta_perc, ba_delta)
+
+# selecting the delta ba (absolute change in area) 
+# that corresponds with the low, median, high % changes in area
+# (note that the median change need not correspond to the median % change
+# and I'm using the % change as the primary variable of interest)
+ba_smry_delta_graze2 <- ba_smry_delta_graze1 %>% 
+  left_join(rename(tmp, ba_delta_median = ba_delta),
+            by = c(join_vars, ba_delta_perc_median = 'ba_delta_perc')) %>% 
+  left_join(rename(tmp, ba_delta_high = ba_delta),
+            by = c(join_vars, ba_delta_perc_high = 'ba_delta_perc')) %>% 
+  left_join(rename(tmp, ba_delta_low = ba_delta),
+          by = c(join_vars, ba_delta_perc_low = 'ba_delta_perc')) %>% 
+  mutate(across(starts_with("ba") & !contains("perc"), ~ round(.x, 0)),
+         across(contains("delta_perc"), ~ round(.x, 1)))
+
+
+# if check's didn't pass then 1:1 joining above failed
+check <- filter(ba_smry_delta_graze2, RCP != 'Current')
+stopifnot(nrow(ba_smry_delta_graze1) == nrow(ba_smry_delta_graze2),
+          all(!is.na(check)))
 
 # save output -------------------------------------------------------------
 
 
-write_csv(ba_smry_delta_graze1,
+write_csv(ba_smry_delta_graze2,
           paste0("data_processed/area/ba_summaries/ba_graze-delta_gcm-wise_", 
                 vr, "_", runv, ".csv"))
 
-write_csv(ba_smry_delta_graze1,
+write_csv(ba_smry_delta_clim1,
           paste0("data_processed/area/ba_summaries/ba_clim-delta_pixel-wise_", 
                  vr, "_", runv, ".csv"))
