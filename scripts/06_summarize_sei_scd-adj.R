1ca5d339813fc4446ea675ab19fa13471649d3d6
Martin Holdrege <martinholdrege@gmail.com>
2025-12-08 10:11:23 -0700

    created gw summaries of q and sei

--- scripts/06_summarize_sei_scd-adj.R
+++ scripts/06_summarize_sei_scd-adj.R
@@ -17,6 +17,8 @@ vr_name <- opt$vr_name
 yr_lab <- opt$yr_lab
 fire_breaks <- c(seq(0, 2.5, by = 0.25), 3, 4) # intervals to cut fire probability into
 
+path_tmp <- file.path('tmp', yr_lab)
+
 # dependencies ------------------------------------------------------------
 
 library(tidyverse)
@@ -28,9 +30,12 @@ source("src/SEI_functions.R")
 # read in data ------------------------------------------------------------
 
  # file create in 05_interpolated_summarize_sei_scd-adj.R
-r_qsei1 <- rast(file.path("data_processed/interpolated_rasters", v_interp,
-                     paste0(runv, yr_lab, "_q-sei_scd-adj_summary.tif")))
+# r_qsei1 <- rast(file.path("data_processed/interpolated_rasters", v_interp,
+#                      paste0(runv, yr_lab, "_q-sei_scd-adj_summary.tif")))
 
+r_q1 <- rast(file.path(path_tmp, 'q_scd_adj1.tif')) # q layers for each GCM
+r_sei1 <- rast(file.path(path_tmp, 'sei_scd_adj1.tif')) # sei layers for each GCM
+r_qsei1 <- c(r_q1, r_sei1)
 eco1 <- load_wafwa_ecoregions(wafwa_only = FALSE, total_region = TRUE,
                               v = vr)
 eco2 <- vect(eco1['ecoregion'])
@@ -40,29 +45,22 @@ r_eco1 <- load_wafwa_ecoregions_raster(wafwa_only = FALSE,
 
 size <- load_cell_size()
 
-# * fire ------------------------------------------------------------------
-# this is for summarizing SEI at different bins of fire probability
-# created in "scripts/04_interpolated_summarize.R"
-r_fire1 <- rast(file.path("data_processed/interpolated_rasters", 
-          paste0(run, "_fire-prob_future_summary_across_GCMs.tif")))
-
-
 # * setup -----------------------------------------------------------------
-
 
 if(test_run) {
   r_qsei1 <- downsample(r_qsei1)
-  r_fire1 <- downsample(r_fire1)
   r_eco1 <- downsample(r_eco1)
   size <- downsample(size)
 }
 
-
 # names of layers --------------------------------------------------
 
-into <- c("group", "type", "RCP", "years", "graze", 'summary')
+into <- c("group", "type", "RCP", "years", "graze", 'GCM')
 
-info1 <- create_rast_info(r_qsei1, into = into) 
+info1 <- create_rast_info(r_qsei1, into = into) %>% 
+  # the 'biomass' layers are actually Q scores 
+  mutate(type = ifelse(type == 'biomass', 'Q', type))
+
 
 # calculate means by ecoregions -------------------------------------------
 
@@ -71,20 +69,39 @@ sixnum <- function(x, na.rm = TRUE) {
   five <- fivenum(x, na.rm = na.rm)
   c(five, mean(x, na.rm = na.rm))
 }
-# 5 number summary by ecoregion
-eco_smry_l1 <- map(info1$id, function(id) {
-  tmp <- terra::extract(r_qsei1[[id]], eco2, sixnum,  na.rm = TRUE)
-  tmp <- as_tibble(tmp)
-  colnames(tmp) <- c('ID', 'min', 'lower', 'middle', 'upper', 'max', 'mean')
-  tmp$'id' <- id
-  tmp$region <- values(eco2)$ecoregion
-  tmp$ID <- NULL
-  tmp
-})
+sixnum_names <- c('min', 'lower', 'middle', 'upper', 'max', 'mean')
+test <- terra::extract(r_qsei1, eco2, sixnum, na.rm = TRUE)
 
-eco_smry1 <- eco_smry_l1 %>% 
-  bind_rows() %>% 
+gcms <- str_replace_all(unique(info1$GCM), '\\-', '\\.')
+regex <- '\\.\\d$'
+regex <- paste0('(?<=(', paste0(gcms, collapse = ')|('), '))\\.\\d$')
+
+eco_gcm1 <- as_tibble(test) %>% 
+  pivot_longer(cols = -ID) %>% 
+  mutate(id = str_replace(name, regex, ''),
+         # output of terra::extract had the '-' replaced by '.', 
+         id = str_replace_all(id, '\\.', '\\-'),
+         stat_num = str_extract(name, regex),
+         stat_num = str_replace(stat_num, '^\\.', ''),
+         stat_num = as.numeric(stat_num),
+         stat_num = ifelse(is.na(stat_num), 0, stat_num) + 1,
+         stat = sixnum_names[stat_num],
+         region = eco2$ecoregion[ID]) 
+
+check <- eco_gcm1 %>% 
+  group_by(id, region) %>% 
+  summarize(n = n(), .groups = 'drop') %>% 
+  pull(n) %>% unique()
+
+stopifnot(check == length(sixnum_names),
+          all(complete.cases(eco_gcm1)))
+
+eco_gcm2 <- eco_gcm1 %>% 
+  select(-stat_num, -ID, -name) %>% 
+  pivot_wider(values_from = 'value',
+              names_from = 'stat') %>% 
   left_join(info1, by = 'id')
+
 
 # calculating % CSA & GOA ------------------------------------------------------
 
@@ -111,7 +128,6 @@ sei_pcent <- left_join(sei_core2, sei_goa2, by = c('region', 'id')) %>%
   left_join(info1, by = 'id') %>% 
   select(-run2)
 
-
 sei_pcent_long <- sei_pcent %>% 
   mutate(percent_ora = 100 - percent_goa - percent_csa) %>% 
   pivot_longer(cols = matches('percent_'),
@@ -121,133 +137,57 @@ sei_pcent_long <- sei_pcent %>%
          c3 = str_to_upper(str_replace(c3, 'percent_', ''))) %>% 
   select(-group, -type, -id)
 
-# calculating C9 transition -----------------------------------------------
-# within a grazing level, and for a given scenario the area falling into
-# each of 9 possible SEI class changes
 
+# gw summaries ------------------------------------------------------------
 
-r_c3 <- sei2c3(r_sei)
+# gcm wise summaries 
 
-info_sei2 <- info_sei1 %>% 
-  filter(RCP == 'Current') %>% 
-  select(-RCP, -years, -run2, -summary, -type) %>% 
-  right_join(filter(info_sei1, RCP != 'Current'),
-             by = join_by(run, group, graze),
-             suffix = c('_cur', '_fut')) 
+group_vars <- c('id', 'region', 'group', 'type', 'RCP', 'years', 'graze')
+gcm_mean <- eco_gcm2 %>% 
+  group_by(across(all_of(group_vars))) %>% 
+  summarize_across_GCMs(var = 'mean') 
 
-r_c9a <- c3toc9(current = r_c3[[info_sei2$id_cur]],
-                future = r_c3[[info_sei2$id_fut]])
+eco_smry_gw0 <- eco_gcm2 %>% 
+  right_join(gcm_mean, 
+             by = c(group_vars, 'mean')) 
 
-r_c9b <- c(r_eco1, size, r_c9a)
+check <- eco_smry_gw %>% 
+  group_by(across(all_of(group_vars))) %>% 
+  summarize(n = n(), .groups = 'drop') %>% 
+  pull(n)
 
-# * area of c9 transition -------------------------------------------------
+stopifnot(all(check == 3)) # check for non-unique GCMs
 
-c9_area1 <- as.data.frame(r_c9b) %>% 
-  rename(region = ecoregion) %>% 
-  mutate(cell_num = 1:n()) %>% 
-  pivot_longer(matches('^fire.*eind'),
-               values_to = 'c9',
-               names_to = "id") %>% 
-  mutate(c9 = c9_factor(c9)) %>% 
-  group_by(id, region, c9) %>% 
-  summarise(area = sum(area),
-            .groups = 'drop_last') %>% 
-  mutate(area_perc = area/sum(area)*100)
+eco_smry_gw <- eco_smry_gw0 %>% 
+  select(-GCM)
 
+tmp <- eco_smry_gw0 %>% 
+  select(region, RCP, years, graze, GCM) %>% 
+  distinct()
 
-# adding in entire study region
-c9_area2 <- c9_area1 %>% 
-  group_by(id, c9) %>% 
-  summarise(area = sum(area), .groups = 'drop_last') %>% 
-  mutate(area_perc = area/sum(area)*100,
-           region = levels(eco1$ecoregion)[1]) %>% 
-  bind_rows(c9_area1) %>% 
-  ungroup() %>% 
-  left_join(info1, by = 'id') %>% 
-  mutate(type = 'c9') %>% 
-  select(-run2)
+sei_pcent_gw <- left_join(tmp, sei_pcent) %>% 
+  select(-GCM)
 
-# check
-test <- c9_area2 %>% 
-  group_by(id, region) %>% 
-  summarize(total = sum(area_perc)) %>% 
-  pull(total)
-
-stopifnot(abs(test - 100) < 0.0001)
-
-# mean SEI by fire probability bin ----------------------------------------
-
-summary4bins <- 'median' # summary across GCMs (pixelwise) used
-info_sei <- info1 %>% 
-  filter(summary == summary4bins, group == 'SEI') %>% 
-  select(-run, -run2, -group, -type)
-
-info_fire1 <- create_rast_info(r_fire1,
-                               into = c("type", "RCP", "years", "graze", 'summary'))
-info_seifire <- info_fire1 %>% 
-  select(-type) %>% 
-  right_join(info_sei, by = c('RCP', 'years', 'graze', 'summary'),
-             suffix = c('_fire', '_sei'))
-
-r_fire2 <- r_fire1[[info_seifire$id_fire]]
-r_sei1 <- r_qsei1[[info_seifire$id_sei]]
-
-# * binning fire ----------------------------------------------------------
-r_comb <- c(r_eco1, r_sei1, r_fire2)
-
-df_seifire1 <- as.data.frame(r_comb)
-
-stopifnot(max(fire_breaks) > max(unlist(minmax(r_fire2))))
-
-df_seifire2 <- df_seifire1 %>% 
-  rename(region = ecoregion) %>% 
-  mutate(cell_num = 1:n()) %>% 
-  pivot_longer(- c(region, cell_num), names_to = 'id') %>% 
-  left_join(bind_rows(info_sei, info_fire1[names(info_sei)]), by = 'id') %>% 
-  mutate(group = str_extract(id, 'SEI|fire-prob')) %>% 
-  select(-id) %>% 
-  pivot_wider(values_from = 'value',names_from = "group") %>% 
-  mutate(fire_bin = cut(`fire-prob`, fire_breaks, right = FALSE)) %>% 
-  # some pixels outside of the regions (from the fire raster)
-  # discarding these
-  filter(!is.na(region))
-
-summarize_sei <- function(df) {
-  summarize(df,
-            n_pixels = n(),
-            percent_csa = percent_csa(SEI),
-            percent_goa = percent_goa(SEI),
-            SEI = mean(SEI),
-            .groups = 'drop')
-}
-
-df_seifire_regions <- df_seifire2 %>% 
-  group_by(region, RCP, years, graze, summary, fire_bin) %>% 
-  summarize_sei()
-
-df_seifire_entire <- df_seifire2 %>% 
-  select(-region) %>% 
-  group_by(RCP, years, graze, summary, fire_bin) %>% 
-  summarize_sei() %>% 
-  mutate(region = eco1$ecoregion[1]) # entire study area
-
-df_seifire3 <- bind_rows(df_seifire_entire, df_seifire_regions)
+sei_pcent_long_gw <- left_join(tmp, sei_pcent_long) %>% 
+  select(-GCM)
 
 # save output -------------------------------------------------------------
 
 prefix <- if(test_run) 'test' else paste0(runv, vr_name, yr_lab)
 
-write_csv(eco_smry1, paste0('data_processed/raster_means/', prefix, 
-                            '_q-sei_scd-adj_summaries_by-ecoregion.csv'))
+write_csv(eco_smry_gw, paste0('data_processed/raster_means/', prefix, 
+                            '_q-sei_scd-adj_smry-gw_by-ecoregion.csv'))
 
-write_csv(sei_pcent , paste0('data_processed/raster_means/', prefix, 
-                            '_sei-class-pcent_scd-adj_summaries_by-ecoregion.csv'))
+write_csv(sei_pcent_gw , paste0('data_processed/raster_means/', prefix, 
+                            '_sei-class-pcent_scd-adj_smry-gw_by-ecoregion.csv'))
 
-write_csv(sei_pcent_long, paste0('data_processed/raster_means/', prefix, 
-                             '_sei-class-pcent-long_scd-adj_summaries_by-ecoregion.csv'))
+write_csv(sei_pcent_long_gw, paste0('data_processed/raster_means/', prefix, 
+                             '_sei-class-pcent-long_scd-adj_smry-gw_by-ecoregion.csv'))
 
-write_csv(df_seifire3, paste0('data_processed/raster_means/', prefix, 
-                              '_sei-by-fire-bin_scd-adj_summaries_by-ecoregion.csv'))
+# see pre Dec 2025 commits for code to create this output
+# write_csv(df_seifire3, paste0('data_processed/raster_means/', prefix, 
+#                               '_sei-by-fire-bin_scd-adj_summaries_by-ecoregion.csv'))
 
-write_csv(c9_area2, paste0('data_processed/raster_means/', prefix, 
-                              '_c9-area_scd-adj_summaries_by-ecoregion.csv'))
+# see pre Dec 2025 commits for code to create
+# write_csv(c9_area2, paste0('data_processed/raster_means/', prefix, 
+#                               '_c9-area_scd-adj_smry-gw_by-ecoregion.csv'))
--- src/gw_functions.R
+++ src/gw_functions.R
@@ -66,3 +66,85 @@ gw_select_smry_site <- function(df, summarize_vars,
   out
   
 }
+
+c3_to_long <- function(sei_byGCM) {
+  c3_gcm2 <- sei_byGCM %>% 
+    mutate(percent_ora = 100 - percent_csa - percent_goa) %>% 
+    select(-matches('SEI_'), -group, -type, -id) %>% 
+    pivot_longer(matches('percent_'), 
+                 values_to = 'c3_percent',
+                 names_to = 'c3') %>% 
+    mutate(c3 = str_replace(c3, 'percent_', ''),
+           c3 = str_to_upper(c3),
+           c3_area = area*c3_percent/100) %>% 
+    select(-area) %>% 
+    df_factor()
+  c3_gcm2
+}
+
+
+# this function creates the file that
+# is the same as 'sei_pcent1' object previously
+# used but with GCM-wise calculations
+summarize_c3_gw <- function(sei_byGCM) {
+  c3_gcm2 <- c3_to_long(sei_byGCM)
+  
+  join_vars <- c('region', 'RCP', 'years', 'graze')
+  
+  # which GCM has low, median or high values of 'mean' SEI
+  # using mean SEI to sort gcms instead of median, because
+  # because in some regions and scenarios median SEI is at or near
+  # 0 so doesn't give seperation between GCMs
+  sei_med_smry <- sei_byGCM %>% 
+    group_by(across(all_of(join_vars))) %>% 
+    summarize_across_GCMs(var = 'SEI_mean') %>% 
+    left_join(sei_byGCM[c(join_vars, 'GCM', 'SEI_mean')],
+              by = c(join_vars, 'SEI_mean')) %>% 
+    filter(!is.na(GCM))
+  
+  test <- sei_med_smry %>% 
+    group_by(across(all_of(join_vars)), summary) %>% 
+    summarise(n = n(), .groups = 'drop')
+  
+  stopifnot(all(test$n == 1))
+  
+  # gcm-wise c3 class area
+  sei_pcent1 <- c3_gcm2 %>% 
+    right_join(sei_med_smry, by = c(join_vars, 'GCM')) %>% 
+    mutate(rcp_year = rcp_label(RCP, years, include_parenth = FALSE,
+                                add_newline = TRUE)) %>% 
+    df_factor()
+  
+  # check if that got a 'unique' gcm for low, median and high
+  test <- sei_pcent1 %>% 
+    group_by(across(all_of(join_vars)), summary, c3) %>% 
+    mutate(n = n(), .groups = 'drop') %>% 
+    pull(n)
+  
+  stopifnot(all(test == 1))
+  
+  sei_pcent1
+}
+
+
+# summarize burned area
+summarize_ba_gw <- function(df_ba_gcm, df_area) {
+  df_area <- rename(df_area, area_total = area)
+  df_ba_gcm2 <- left_join(df_ba_gcm, df_area,
+                          by = 'ecoregion')
+  
+  df_ba_gcm3 <- df_ba_gcm2 %>% 
+    group_by(ecoregion, RCP, years, graze, area_total) %>% 
+    summarize_across_GCMs(var = 'area')
+  
+  stopifnot(with(df_ba_gcm3, any(!is.na(area) & RCP != 'Current')))
+  
+  df_ba_gcm3 %>% 
+    pivot_wider(values_from = c('area'),
+                names_from = 'summary',
+                names_prefix = 'area_') %>% 
+    mutate(across(c('area_low', 'area_median', 'area_high'),
+                  .fns = list('perc' = \(x) x/area_total*100)),
+           rcp_year = rcp_label(RCP, years, include_parenth = FALSE)) %>% 
+    df_factor()
+}
